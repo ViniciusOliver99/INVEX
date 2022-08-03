fcb285900a0d659b13b963721dc154b6b1db842d
Vinicius <109105516+ViniciusOliver99@users.noreply.github.com>
2022-08-03 18:40:15 -0300

    Delete invex001 directory

--- invex001/contas/forms.py
+++
@@ -1,8 +0,0 @@
-from django.forms import ModelForm
-from .models import Transacao
-
-
-class TransacaoForm(ModelForm):
-    class Meta:
-        model = Transacao
-        fields = ['descricao', 'valor', 'categoria']
